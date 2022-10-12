6207413d8351fd502dc3c02bdb37eecc18f7d6fb
pranshunagar01 <59359937+pranshunagar01@users.noreply.github.com>
2022-10-11 21:35:31 -0500

    Update index.js

--- procolab-frontend/src/index.js
+++ procolab-frontend/src/index.js
@@ -1,9 +1,14 @@
+// Import react and react-dom libraries
 import React from 'react';
 import {render} from 'react-dom';
+
+// Import auth0 library
 import { Auth0Provider } from '@auth0/auth0-react';
+
+// Import App component
 import App from './App';
 
-
+// Render App component with Auth0Provider
 render(
   <Auth0Provider
     domain=""
