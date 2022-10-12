dfb6248392ac7a0e3bc7319b701348818af093d1
pranshunagar01 <59359937+pranshunagar01@users.noreply.github.com>
2022-10-11 21:21:40 -0500

    Update index.js

--- procolab-backend/index.js
+++ procolab-backend/index.js
@@ -1,16 +1,40 @@
+/* procolab-backend v1.0.0 - 2022-09-09 */
+
+/* This is the main backend file for Procolab. It handles all the basic requests and responses from the client. */
+
+/* Import Express as a dependency */
 const express = require('express');
+
+/* Create an instance of Express */
 const app = express();
+
+/* body-parser is a middleware that parses the body of the request */
 const bodyParser = require('body-parser');
+
+/* cors is a middleware that allows cross-origin requests */
 const cors = require('cors');
+
+/* Use the body-parser middleware */
 app.use(bodyParser.urlencoded({extended: true}));
+
+/* Use cors to allow cross-origin requests */
 app.use(cors());
+
+/* Mongoose is a MongoDB object modeling tool designed to work in an asynchronous environment */
 const mongoose = require('mongoose');
+
+/* Use json parser */
 app.use(express.json());
+
+
 app.use(express.static('build'))
+
+/* Connect to the database */
 mongoose.connect(
     "",
     { useNewUrlParser: true }
   );
+  /* Set the headers for the response */
   app.use(function(req,res,next){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -18,6 +42,8 @@ mongoose.connect(
     res.header("Access-Control-Allow-Credentials", true);
     next();
 });
+
+/* Schema for the database */
 const projectSchema = new mongoose.Schema({
     _id: {
         type: String,
@@ -54,13 +80,16 @@ const projectSchema = new mongoose.Schema({
     }
 })
 
+/* Model for the database */
 const Project = mongoose.model("Project", projectSchema);
 
+/* View the requests */
 app.post("/viewReq", (req, res)=>{
     console.log(req.body);
     res.send(req.body);
 })
 
+/* Create a new project */
 app.post("/newProject", (req, res)=>{
     const newProj = new Project({
         _id: req.body.id,
@@ -81,6 +110,8 @@ app.post("/newProject", (req, res)=>{
         }
     });
 });
+
+/* Get the project */
 app.get("/readProjectsAdmin/:name", (req, res)=>{
     Project.find({admin: {email: req.params.name, permissions: 100, admin: true}}, (err, elem)=>{
         if(err){
@@ -91,6 +122,8 @@ app.get("/readProjectsAdmin/:name", (req, res)=>{
         }
     })
 })
+
+/* Get a particular project */
 app.get("/particularProject/:id", (req, res)=>{
     Project.find({_id: req.params.id}, function(err, elem){
         if(err){
@@ -102,6 +135,8 @@ app.get("/particularProject/:id", (req, res)=>{
         }
     })
 })
+
+/* Get the project collaborators */
 app.get("/readProjectsCollaborator/:name", (req, res)=>{
     Project.find({$or: [{ collaborators:  {email: req.params.name, permissions: 100, admin: false}}, { collaborators:  {email: req.params.name, permissions: 40, admin: false}}, { collaborators:  {email: req.params.name, permissions: 0, admin: false}}, { collaborators:  {email: req.params.name, permissions: 100, admin: true}}]}, (err, elem)=>{
         if(err){
@@ -112,6 +147,8 @@ app.get("/readProjectsCollaborator/:name", (req, res)=>{
         }
     })
 })
+
+/* Update the project code */
 app.post("/updateCode", (req, res)=>{
     Project.updateOne({_id: req.body.id}, {code: req.body.newCode}, function(err){
         if(err){
@@ -122,6 +159,8 @@ app.post("/updateCode", (req, res)=>{
         }
     })
 })
+
+/* Remove a project collaborator */
 app.post("/removeCollaborator", (req, res)=>{
     Project.update(
         {_id: req.body.id},
@@ -132,6 +171,8 @@ app.post("/removeCollaborator", (req, res)=>{
         }
     )
 })
+
+/* Remove a project admin */
 app.post("/removeAdmin", (req, res)=>{
     Project.update(
         {_id: req.body.id},
@@ -142,6 +183,8 @@ app.post("/removeAdmin", (req, res)=>{
         }
     )
 })
+
+/* Delete a project */
 app.post("/deleteProject", (req, res)=>{
     Project.deleteMany({_id: req.body.id}, function(error){
         if(error){
@@ -152,6 +195,8 @@ app.post("/deleteProject", (req, res)=>{
         }
     })
 })
+
+/* Change permissions of a collaborator */
 app.post("/changeCollaboratorPermissions", (req, res)=>{
     Project.find(
         {_id: req.body.id, "collaborators.email": req.body.collaboratorName},
@@ -184,6 +229,8 @@ app.post("/changeCollaboratorPermissions", (req, res)=>{
         }
     )
 })
+
+/* Add a message to the chat database */
 app.post("/addMessageToChat", (req, res)=>{
     let newMessage = {
         author: req.body.author,
@@ -203,7 +250,9 @@ app.post("/addMessageToChat", (req, res)=>{
         }
     )
 })
+
+/* Start the server at port 3002 */
+/* process.env.PORT is for heroku. It will automatically assign a port which will be used by the server */
 app.listen(process.env.PORT||3002, ()=>{
     console.log("The server is up and running at 3002");
 })
-
